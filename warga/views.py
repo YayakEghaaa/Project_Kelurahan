e7656b0aa2169e51ec964f1a49c07fbcbacb853b
YayakEghaaa <yayakegha@gmail.com>
2025-10-14 12:53:27 +0700

    feat: pertemuan 2 - implementasi model Pengaduan dengan relasi ForeignKey, PengaduanListView, dan tampilan daftar pengaduan per warga

--- warga/views.py
+++ warga/views.py
@@ -1,10 +1,16 @@
 from django.views.generic import ListView, DetailView
 from .models import Warga
+from .models import Warga, Pengaduan
 
 class WargaListView(ListView):
-    model = Warga
-    template_name = 'warga/warga_list.html'  # Tambahkan ini untuk eksplisit
+    model         = Warga
+    template_name = 'warga/warga_list.html' 
 
 class WargaDetailView(DetailView):
-    model = Warga
-    template_name = 'warga/warga_detail.html'  # Tambahkan ini untuk eksplisit
+    model         = Warga
+    template_name = 'warga/warga_detail.html'
+    
+class PengaduanListView(ListView):
+    model               = Pengaduan
+    template_name       = 'Warga/pengaduan_list.html'
+    context_object_name = 'daftar_pengaduan'
